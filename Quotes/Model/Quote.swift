c2934f0f9ae32f455cfbc12537254a463d42a1d0
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-02-23 11:00:09 -0500

    Done

--- Quotes/Model/Quote.swift
+++ Quotes/Model/Quote.swift
@@ -7,10 +7,7 @@
 
 import Foundation
 
-struct Quote: Decodable {
+struct Quote: Decodable, Hashable {
     let quoteText: String
     let quoteAuthor: String
-    let senderName: String
-    let senderLink: String
-    let quoteLink: String
 }
--- Quotes/Views/ContentView.swift
+++ Quotes/Views/ContentView.swift
@@ -10,23 +10,24 @@ import SwiftUI
 struct ContentView: View {
     // MARK: Stored properties
     @State var currentQuote: Quote = Quote(quoteText: "",
-                                           quoteAuthor: "",
-                                           senderName: "",
-                                           senderLink: "",
-                                           quoteLink: "")
-    
+                                           quoteAuthor: "")
+    @State var favourites: [Quote] = []
+    @State var currenQuoteAddedToFavourites = false
     // MARK: Computed properties
     var body: some View {
         VStack {
             VStack{
                 Text(currentQuote.quoteText)
-                    .multilineTextAlignment(.leading)
-                    .padding(30)
+                    .font(.title)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
                 HStack {
                     Spacer()
                     Text("-")
                     Text(currentQuote.quoteAuthor)
+                        .font(.body)
                         .italic()
+                        .minimumScaleFactor(0.5)
                 }
                 .padding(10)
             }
@@ -38,8 +39,17 @@ struct ContentView: View {
             Image(systemName: "heart.circle")
                 .resizable()
                 .frame(width: 35, height: 35)
+                .foregroundColor(currenQuoteAddedToFavourites ? .red : .secondary)
+                .onTapGesture {
+                    if !currenQuoteAddedToFavourites {
+                        favourites.append(currentQuote)
+                        currenQuoteAddedToFavourites = true;
+                    }
+                }
             Button(action: {
-                print("")
+                Task {
+                    await loadNewQuote()
+                }
             }, label: {
                 Text("Another one!")
             })
@@ -49,55 +59,64 @@ struct ContentView: View {
                     .bold()
                 Spacer()
             }
-            List {
-                Text("Nothing is a waste of time.")
-                Text("If your actions inspire others.")
+            List(favourites, id: \.self) { currentQuote in
+                VStack{
+                    Text(currentQuote.quoteText)
+                        .minimumScaleFactor(0.5)
+                        .multilineTextAlignment(.center)
+                    HStack {
+                        Spacer()
+                        Text("-")
+                        Text(currentQuote.quoteAuthor)
+                            .italic()
+                    }
+                }
             }
             Spacer()
             
         }
         // When the app opens, get a new joke from the web service
         .task {
-            
-            // Assemble the URL that points to the endpoint
-            let url = URL(string: "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en")!
-            
-            // Define the type of data we want from the endpoint
-            // Configure the request to the web site
-            var request = URLRequest(url: url)
-            // Ask for JSON data
-            request.setValue("application/json",
-                             forHTTPHeaderField: "Accept")
-            
-            // Start a session to interact (talk with) the endpoint
-            let urlSession = URLSession.shared
-            
-            // Try to fetch a new joke
-            // It might not work, so we use a do-catch block
-            do {
-                
-                // Get the raw data from the endpoint
-                let (data, _) = try await urlSession.data(for: request)
-                
-                // Attempt to decode the raw data into a Swift structure
-                // Takes what is in "data" and tries to put it into "currentQuote"
-                //                                 DATA TYPE TO DECODE TO
-                //                                         |
-                //                                         V
-                currentQuote = try JSONDecoder().decode(Quote.self, from: data)
-                
-            } catch {
-                print("Could not retrieve or decode the JSON from endpoint.")
-                // Print the contents of the "error" constant that the do-catch block
-                // populates
-                print(error)
-            }
-            
+            await loadNewQuote()
         }
         .navigationTitle("Quotes")
         .padding()
     }
-    
+    func loadNewQuote() async {
+        // Assemble the URL that points to the endpoint
+        let url = URL(string: "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en")!
+        
+        // Define the type of data we want from the endpoint
+        // Configure the request to the web site
+        var request = URLRequest(url: url)
+        // Ask for JSON data
+        request.setValue("application/json",
+                         forHTTPHeaderField: "Accept")
+        
+        // Start a session to interact (talk with) the endpoint
+        let urlSession = URLSession.shared
+        
+        // Try to fetch a new joke
+        // It might not work, so we use a do-catch block
+        do {
+            
+            // Get the raw data from the endpoint
+            let (data, _) = try await urlSession.data(for: request)
+            
+            // Attempt to decode the raw data into a Swift structure
+            // Takes what is in "data" and tries to put it into "currentQuote"
+            //                                 DATA TYPE TO DECODE TO
+            //                                         |
+            //                                         V
+            currentQuote = try JSONDecoder().decode(Quote.self, from: data)
+            
+        } catch {
+            print("Could not retrieve or decode the JSON from endpoint.")
+            // Print the contents of the "error" constant that the do-catch block
+            // populates
+            print(error)
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
