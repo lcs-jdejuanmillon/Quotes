5c1715287ea505b084c5c995e1018b0e28df8f28
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-02-22 22:00:19 -0500

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Jacobo de Juan Millon on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
